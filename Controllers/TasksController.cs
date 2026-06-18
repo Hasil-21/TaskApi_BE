205ada53aeb5bbe79372be73dd30829d4b197065
Hasil Trivedi <PCA56@server7.com>
2026-06-18 15:01:13 +0530

    new commit

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -1,10 +1,10 @@
+using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TaskApi.Data;
 using TaskApi.Models;
-using Amazon.SQS;
-using Amazon.SQS.Model;
-using System.Text.Json;
 
 namespace TaskApi.Controllers;
 
@@ -14,23 +14,17 @@ public class TasksController : ControllerBase
 {
     private readonly AppDbContext _db;
     private readonly IAmazonSQS _sqs;
-    public TasksController(AppDbContext db,IAmazonSQS sqs)
+    public TasksController(AppDbContext db, IAmazonSQS sqs)
     {
-        _db=db;
-        _sqs=sqs;
+        _db = db;
+        _sqs = sqs;
     }
 
     [HttpGet("{id}")]
     public async Task<IActionResult> GetById(int id)
     {
         var task = await _db.Tasks.FindAsync(id);
-        return task is null ? NotFound() : Ok(
-            await _sqs.SendMessageAsync(new SendMessageRequest
-            {
-                QueueUrl = "https://sqs.ap-south-1.amazonaws.com/292578125952/demo-queue",
-                MessageBody = JsonSerializer.Serialize(task)
-            })
-        );
+        return task is null ? NotFound() : Ok(task);
     }
 
     [HttpPost]
@@ -38,6 +32,11 @@ public class TasksController : ControllerBase
     {
         _db.Tasks.Add(task);
         await _db.SaveChangesAsync();
+        await _sqs.SendMessageAsync(new SendMessageRequest
+        {
+            QueueUrl = "https://sqs.ap-south-1.amazonaws.com/292578125952/demo-queue",
+            MessageBody = JsonSerializer.Serialize(task)
+        });
         return CreatedAtAction(nameof(GetById), new { id = task.Id }, task);
     }
 
