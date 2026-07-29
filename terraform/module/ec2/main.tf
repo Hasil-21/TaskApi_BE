5cab4258dfa13fb8f59fbdfd096523929ebfd21b
Hasil-21 <hasiltrivedi21@gmail.com>
2026-07-29 11:45:50 +0000

    changes to SG

--- terraform/module/ec2/main.tf
+++ terraform/module/ec2/main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "ec2_sg"{
         to_port = 22
         from_port = 22
         protocol = "tcp"
-        cidr_blocks = [var.my_ip]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
